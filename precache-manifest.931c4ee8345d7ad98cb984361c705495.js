774a0a38e5f295f64d0988e13d95c579fc1698cb
Ameet Gohil <qubits1@gmail.com>
2020-03-26 14:58:34 -0700

    Updates

---
+++ precache-manifest.931c4ee8345d7ad98cb984361c705495.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ffb75fe9537b179550b54494f34838",
+    "url": "/electron-config-react/index.html"
+  },
+  {
+    "revision": "3768a74abe8b3a74dfb2",
+    "url": "/electron-config-react/static/css/main.7ed36b81.chunk.css"
+  },
+  {
+    "revision": "c9777a5a48652e8ff8fc",
+    "url": "/electron-config-react/static/js/2.29f3c088.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/electron-config-react/static/js/2.29f3c088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3768a74abe8b3a74dfb2",
+    "url": "/electron-config-react/static/js/main.28b3424c.chunk.js"
+  },
+  {
+    "revision": "392536fd511d0760ee46",
+    "url": "/electron-config-react/static/js/runtime-main.f65b6d6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/electron-config-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
